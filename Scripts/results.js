50f39fce2c231d7c97154320dc265c6cd7a39ffa
Jason Clifton <j.clifton1473@gmail.com>
2017-05-19 09:23:27 -0600

    deleted extra files

--- Scripts/results.js
+++ Scripts/results.js
@@ -1,3 +1,27 @@
+$(appReady);
+
+function appReady() {
+  hideElements()
+  clickFoodItem();
+  removeFromList();
+  $('#macroSubmit').submit(getUserMacros)
+  $('#searchSubmit').submit(submitForm);
+  $('#searchSubmit').submit(function() {
+    $('html, body').stop(true, true).animate({
+      scrollTop: $('table').delay(300).offset().top
+    }, 1500);
+  });
+  $('#idLink').on("click", toggleAbout)
+  $('.dismiss').on('click', function() {
+    $('#about').hide();
+  })
+  $('.clear-search').click(function() {
+    $('.nutritionFacts').empty();
+    $('table').hide()
+  })
+
+}
+
 function hideElements() {
   $('#searchSubmit').hide();
   $table.hide();
